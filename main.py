b728cfb1139a4b5770e611e381325dd7886d3f5a
DEVELoper <syabblogger@gmail.com>
2022-12-13 21:11:30 +0500

    Initial commit

---
+++ main.py
@@ -0,0 +1,25 @@
+print("************************************************")
+print("************************************************")
+print("************************************************")
+print("****DDDD********EEEEEEEEE*VV______________VV****")
+print("****D___DDD*****E________**VV___________VV******")
+print("****D______DD***E________***VV_________VV*******")
+print("****D______DD***EEEEEEEEE****VV_______VV********")
+print("****D______DD***EEEEEEEEE*****VV_____VV*********")
+print("****D______DD***E________******VV___VV****SSSSS*")
+print("****D___DDD*****E________*******VV_VV******SSS**")
+print("****DDDD********EEEEEEEEE********VV*******SSSSS*")
+print("******___________________________________*******")
+print("***************________________*****************")
+print("*********************____***********************")
+
+
+print("Welcome to Developer World")
+print("This is Developer Here")
+print("Hey Folks, Let me Tell You SomeThing Before using this tool\n this is for educational use only \n i will not be responcibal for any illegal work")
+print("First of all Subscribe to My YouTube Channel Here")
+print("YouTube Channel: Real Coder # LINK")
+
+print("1: Python")
+print("2: metaspoit")
+
